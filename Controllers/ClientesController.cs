d37b8a911cf4b0883a70f78c84e00fbbe6cc90f4
thiago araujo <devthiagoaraujo@gmail.com>
2024-01-04 02:53:50 -0300

    Add project files.

---
+++ Controllers/ClientesController.cs
@@ -0,0 +1,93 @@
+﻿using CadastroClientes.Models;
+using CadastroClientes.Models.Repository;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Identity.Client;
+
+namespace CadastroClientes.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClientesController : ControllerBase
+    {
+        [HttpPost("Salvar")]
+        public object Salvar([FromBody] Clientes cadastro)
+        {
+            try
+            {
+                ClientesRepository clilentes = new ClientesRepository();
+                clilentes.Salvar(cadastro);
+            }
+            catch (Exception ex)
+            {
+
+            }
+
+            return null;
+        }
+
+        [HttpPost("Alterar")]
+        public object Alterar([FromBody] Clientes cadastro)
+        {
+            try
+            {
+                
+            }
+            catch (Exception ex)
+            {
+
+            }
+
+            return null;
+        }
+
+        [HttpGet("Listar")]
+        public object Listar()
+        {
+            List<Clientes> listaCli = null;
+            try
+            {
+                ClientesRepository clientesRepo = new ClientesRepository();
+                listaCli = clientesRepo.Listar();
+            }
+            catch (Exception ex)
+            {
+
+            }
+
+            return listaCli;
+        }
+
+        [HttpDelete("Deletar")]
+        public object Deletar(string Documento)
+        {
+            try
+            {
+                ClientesRepository clientes = new ClientesRepository();
+                bool retornoDelete = clientes.Deletar(Documento);
+
+                return retornoDelete;
+            }
+            catch (Exception ex)
+            {
+
+            }
+
+            return null;
+        }
+
+        [HttpGet("GetCliente")]
+        public object GetCliente(string Documento) {
+            try {
+                ClientesRepository cliente = new ClientesRepository();
+                var retorno = cliente.GetCliente(Documento);
+                return retorno;
+            }
+            catch (Exception ex) {
+
+            }
+
+            return null;
+        }
+    }
+}
---
+++ Models/Repository/ClientesRepository.cs
@@ -0,0 +1,55 @@
+﻿using Newtonsoft.Json;
+
+namespace CadastroClientes.Models.Repository
+{
+    public class ClientesRepository
+    {
+        public void Salvar(Clientes clientes)
+        {
+            var listaClientes = Listar();
+            var item = listaClientes.Where(t => t.Documento == clientes.Documento).FirstOrDefault();
+
+            if (item != null) 
+            {
+                Deletar(clientes.Documento);
+            }
+
+            var clientesTexto = JsonConvert.SerializeObject(clientes) + "," + Environment.NewLine;
+            File.AppendAllText("C:\\Users\\devth\\OneDrive\\Documentos\\CadastroBaseClientes\\BancoDados\\bancodados.txt", clientesTexto);
+        }
+
+        public List<Clientes> Listar()
+        {
+            var clientes = File.ReadAllText("C:\\Users\\devth\\OneDrive\\Documentos\\CadastroBaseClientes\\BancoDados\\bancodados.txt");
+            
+            List<Clientes> clientesLista = JsonConvert.DeserializeObject<List<Clientes>>("["+clientes+"]");
+
+            return clientesLista.OrderByDescending(t=>t.Nome).ToList();
+        }
+
+        public bool Deletar(string Documento) 
+        {
+            var listaClientes = Listar();
+            var item = listaClientes.Where(t => t.Documento == Documento).FirstOrDefault();
+            if(item != null) {
+                listaClientes.Remove(item);
+                File.WriteAllText("C:\\Users\\devth\\OneDrive\\Documentos\\CadastroBaseClientes\\BancoDados\\bancodados.txt", string.Empty);
+
+                foreach(var cliente in listaClientes) {
+                    Salvar(cliente);
+                }
+
+                return true;
+            }
+
+            return false;
+        }
+
+        public Clientes GetCliente(string Documento) {
+            var clienteLista = Listar();
+            var item = clienteLista.Where(t=>t.Documento == Documento).FirstOrDefault();
+
+            return item;
+        }
+    }
+}
